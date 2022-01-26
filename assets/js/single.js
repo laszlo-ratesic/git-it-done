1983a0737be2fa61869dc8f3a8746dabfc88e7cb
laszlo-ratesic <keenanrc@hotmail.com>
2022-01-25 21:16:18 -0600

    add error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,18 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split('=')[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace('./index.html');
+    }
+}
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,8 +32,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+        document.location.replace('./index.html');
     }
   });
 };
@@ -39,12 +50,12 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -61,7 +72,7 @@ var displayIssues = function(issues) {
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -81,4 +92,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
